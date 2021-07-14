a6127512bb7b568230920c946d81a5623da09e87
Leonardo Padilha <leoprsouza@hotmail.com>
2021-07-13 23:28:23 -0300

    Lambdas e Functions pt 2

--- CursoJava/src/lambdas/Calculo.java
+++ CursoJava/src/lambdas/Calculo.java
@@ -1,7 +1,18 @@
 package lambdas;
 
+@FunctionalInterface
 public interface Calculo {
 
 	double executar( double a, double b);
+	
+	//Tem a possibilidade de criar um metodo default
+	default String legal() {
+		return "legal";
+	}
+	
+	//Assim como tambem um estatico 
+	static String muitoLegal() {
+		return "Muito legal";
+	}
 	
 }
